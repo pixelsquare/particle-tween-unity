2ee5bd3dca7a40bf72bc81406b1f1784f97050e2
Anthony Ganzon <anthony_0205@yahoo.com>
2018-05-08 21:53:23 +0800

    Initial commit

---
+++ Assets/Arc.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arc : MonoBehaviour 
+{
+	[SerializeField] private Transform m_target = null;
+	[SerializeField] private float m_speed = 1f;
+
+	private Transform m_transform = null;
+
+	public void Awake()
+	{
+		m_transform = transform;
+	}
+
+	public void Update()
+	{
+		Vector3 position = m_transform.position;
+
+		if(Input.GetMouseButtonDown(0))
+		{
+						Debug.Log(Screen.height * Input.mousePosition.x + Screen.width * Input.mousePosition.y - Screen.width * Screen.height);
+
+			Vector3 startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			startPos.z = 0f;
+			position = startPos;
+
+			float v = Screen.height * Input.mousePosition.x + Screen.width * Input.mousePosition.y - Screen.width * Screen.height;
+
+			if(v > 0)
+			{
+				transform.rotation = Quaternion.Euler(Vector3.forward * 30f);
+			}
+			else if(v < 0)
+			{
+				transform.rotation = Quaternion.Euler(Vector3.forward * 210f);
+			}
+		}
+
+		Vector3 targetPos = (m_target.position - position);
+		float angle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
+		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime);
+
+		position += transform.right * Time.deltaTime * m_speed;
+
+
+
+
+		m_transform.position = position;
+	}
+}
---
+++ Assets/Circular.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Circular : MonoBehaviour
+{
+	[SerializeField] private float m_speed = 5f;
+	[SerializeField] private float m_amplitude = 5f;
+
+	private Transform m_transform = null;
+
+	public void Awake()
+	{
+		m_transform = transform;
+	}
+
+	public void Update()
+	{
+		Vector3 position = m_transform.position;
+		position.x = m_amplitude * Mathf.Sin(Time.time * m_speed * Mathf.PI);
+		position.y = m_amplitude * Mathf.Cos(Time.time * m_speed * Mathf.PI);
+		m_transform.position = position;
+	}
+}
